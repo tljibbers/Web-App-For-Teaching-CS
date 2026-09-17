83123a434347bfa83253bb2f0c331ab7f48adfa1
tljibbers <aahijoh@gmail.com>
2026-09-17 16:38:26 -0700

    initial commit + header created

---
+++ web-app-for-teaching-cs/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="headerBox">
+        <div id="website-title">Website Title</div>
+        <div className="Lesson-GetStarted">
+            <button> Lessons </button>
+            <button> Roadmap </button>
+            <button> Games </button>
+            <button> About </button>
+            <button> Get Started </button>
+        </div>
+    </div>
+  );
+}
+export default Header;
---
+++ web-app-for-teaching-cs/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Header from './Header.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+  </StrictMode>,
+)
