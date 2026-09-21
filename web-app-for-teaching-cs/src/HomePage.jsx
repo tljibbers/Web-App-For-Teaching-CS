51648c7a93d38302bb4140ace58dae8a18b97e11
tljibbers <aahijoh@gmail.com>
2026-09-20 21:00:02 -0700

    made the pages that are attributed to the header buttons

---
+++ web-app-for-teaching-cs/src/HomePage.jsx
@@ -0,0 +1,21 @@
+import Header from './Header'
+
+function HomePage() {
+  return (
+    <>
+        <Header />
+        <div>
+           <p>Learn Computer Science in a fun, engaging way.</p>
+           <p>This website was designed specifically for anyone who shows an interest in Computer Science, but doesn't know where to start.</p>
+            
+        </div>
+
+        <div>
+            <p>Made for Visual and Kinesthetic Learners</p>
+            <p>This website guarantees that it will help those who need a more hands on experience.</p>
+        </div>
+    </>
+  );
+}
+
+export default HomePage;
--- web-app-for-teaching-cs/src/main.jsx
+++ web-app-for-teaching-cs/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './Header.jsx'
 import './index.css'
 import './header.css'
+import HomePage from './HomePage'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
+    <HomePage />
   </StrictMode>,
 )
