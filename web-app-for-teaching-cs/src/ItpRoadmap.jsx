61eeb16d3f84824183df1bd1c6982e293da242da
tljibbers <aahijoh@gmail.com>
2026-09-24 17:49:29 -0700

    added icons to roadmap

--- web-app-for-teaching-cs/src/ItpRoadmap.jsx
+++ web-app-for-teaching-cs/src/ItpRoadmap.jsx
@@ -2,13 +2,24 @@ import {useNavigate} from 'react-router-dom';
 import Header from './Header'
 import RoadmapButton from './RoadmapButton'
 import './roadmapButton.css'
+import { LineConnector } from './RoadmapButton'
 
 function ItpRoadmap() {
     const navigate = useNavigate();
     return (
         <>
             <Header />
-            <RoadmapButton name="Hello World!" available={true} />
+            <RoadmapButton name="Hello World!" available={false} id="hello-world" />
+            <LineConnector x1="90" y1="100" x2="250" y2="10" />
+            <RoadmapButton name="Variables" available={false} id="variables" />
+            <RoadmapButton name="Operators" available={false} id="operators" />
+            <RoadmapButton name="Functions" available={false} id="functions" />
+            <RoadmapButton name="Arrays" available={true} id="arrays" />
+            <RoadmapButton name="Data Types" available={false} id="data-types" />
+            <RoadmapButton name="Type Casting" available={false} id="type-casting" />
+            <RoadmapButton name= "If/Else/Then" available={true} id="if-else-then" />
+            <RoadmapButton name="Loops" available={false} id="loops" />
+            <RoadmapButton name="Recursion" available={false} id="recursion" />
         </>
     )
 }
--- web-app-for-teaching-cs/src/RoadmapButton.jsx
+++ web-app-for-teaching-cs/src/RoadmapButton.jsx
@@ -1,10 +1,11 @@
 import hexagonImagePlain from './assets/hexagon-fill-svgrepo-com.svg';
 import hexagonImageAvailable from './assets/hexagon-green.svg';
+import lockImage from './assets/lock-svgrepo-com.svg';
 
 function RoadmapButton(props){
     if (props.available === true) {
         return (
-            <div className="roadmapButtonContainer">
+            <div className="roadmapButtonContainer" id={props.id}>
                 <img src={hexagonImageAvailable} className="roadmapButton" width="120" height="116"></img>
                 <div className="roadmapButtonText">{props.name}</div>
             </div>
@@ -12,11 +13,20 @@ function RoadmapButton(props){
     }
 
     return (
-        <div className="roadmapButtonContainer">
+        <div className="roadmapButtonContainer" id={props.id}>
             <img src={hexagonImagePlain} className="roadmapButton" width="120" height="116"></img>
-            <div className="roadmapButtonText">{props.name}</div>
+            <img src={lockImage} className="lockImage" width="20" height="20"></img>
+            <div className="roadmapButtonTextLocked">{props.name}</div>
         </div>
     );
+}
+
+export function LineConnector(props) {
+    return (
+        <svg width="200" height="100">
+            <line x1={props.x1} y1={props.y1} x2={props.x2} y2={props.y2} stroke="#FFFFFF" strokeWidth="3" />
+        </svg>
+    )
 }
 
 export default RoadmapButton;
